33cbfa465b84e5c1eb998fe67f7b45dfe8ba318d
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-05 13:04:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,13 +28,7 @@ ACC_ALL_CHAT = "-1001866499414"
 START_MSG = "<b>Hᴇʏ {} Iᴀᴍ Pʀɪᴠᴀᴛᴇ Bᴏᴛ Mᴀꜱᴛᴇʀ Oғ Tᴏ Uꜱᴇ Aᴜᴛᴏ Dᴇʟᴇᴛᴇ Aᴜᴛᴏ Aᴄᴄᴇᴘᴛ Aɴᴅ Mᴏʀᴇ...</b>\n<b>Iᴀᴍ Oғғɪᴄɪᴀʟʏ Wᴏʀᴋɪɴɢ Fᴏʀ Fɪʟɪᴍ Hᴏᴍᴇ Gʀᴏᴜᴘ</b>\n<b>Dᴏɴ'ᴛ Wᴀꜱᴛᴇ Yᴏᴜʀ Tɪᴍᴇ Tᴏ Aᴅᴅɪɴɢ Yᴏᴜʀ Gʀᴏᴜᴘ.. Iᴀᴍ Wᴏʀᴋꜱ Oɴʟʏ Mʏ Gʀᴏᴜᴘ</b>"
 
 
-User = Client(name="user-account",
-              session_string=SESSION,
-              api_id=API_ID,
-              api_hash=API_HASH,
-              #Removed
-              workers=300
-              )
+
 
 
 Client = Client(name="auto-delete",
@@ -74,52 +68,8 @@ async def start(bot, message):
     
         
         
-@User.on_message(filters.chat(GROUPS))
-async def delete(user, message):
-    try:
-       if message.from_user.id in ADMINS:
-          return
-       else:
-          #await User.send_reaction(message.chat.id, message.id, "🔥")
-          await asyncio.sleep(TIME)
-          await Client.delete_messages(message.chat.id, message.id)
-    except Exception as e:
-       print(e)
-   
-@Client.on_message(filters.command("deleteallm") & filters.user(ADMINS) & filters.group)
-async def accehpt(bot, message):
-    await message.reply(
-        text="Proccesing"
-    )
-    #Hello
-    numbers = []
-    while True:
-        try:
-            async for m in User.iter_history(msg.chat.id):
-                numbers.append(m.message_id)
-            break
-        except Exception as e:
-           print(e)
-    id_lists = [numbers[i*100:(i+1)*100] for i in range((len(numbers)+100-1) // 100)]
-    for id_list in id_lists:
-        while True:
-            try:
-                await User.delete_messages(msg.chat.id, id_list)
-            except Exception as e:
-               print(e)
- 
 
-@Client.on_message(filters.command("run") & filters.user(ADMINS) & filters.private)
-async def accept(bot, message):
-    lol = await message.reply_text(
-        text ='Proccesing'
-    )
-    chat=ACC_ALL_CHAT # Chat
-    user=message.from_user # User
-    await User.approve_all_chat_join_requests(chat_id=ACC_ALL_CHAT)
-    await lol.edit(f"Completed")
-    print("Completed")
-
+    
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(client: Client, message: Message):
     chat=message.chat # Chat
@@ -147,27 +97,7 @@ async def dbtool(_, m : Message):
     
 
 
-@Client.on_message(filters.command('wt') & filters.user(ADMINS))
-async def clean(bot, message):
-    data = message.text
-    command, nomber = data.split(" ")
-    await message.reply_text(f"https://api.whatsapp.com/send?phone={nomber}")
 
-    
-        
-
-@Client.on_message(filters.command("send") & filters.user(ADMINS))
-async def deletemultiplefiles(bot, message):
-    btn = [[
-            InlineKeyboardButton("Tɪᴍᴇ ⏰", callback_data="time"),
-            InlineKeyboardButton("Dᴀᴛᴇ 📅", callback_data="date")
-          ]]
-    await message.reply_text(
-        text="<b>ɢʀᴏᴜᴘ‌ ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ᴀғᴛᴇʀ 𝟷𝟻 ᴍɪɴᴜᴛᴇs ᴅᴜᴇ ᴛᴏ ᴄᴏᴘʏʀɪɢʜᴛ ɪssᴜᴇ.\n⚜ ᴘᴏᴡᴇʀᴇᴅ ʙʏ\n\n@Sinimapremi </b>",
-        reply_markup=InlineKeyboardMarkup(btn)
-    )
-
-    
     
 @Client.on_message(filters.command("bcast") & filters.user(ADMINS))
 async def bcast(_, m : Message):
@@ -202,34 +132,7 @@ async def bcast(_, m : Message):
     
   
 
-@Client.on_message(filters.command('time') & filters.private)
-async def start(bot, message):
-    data = message.text
-    command, timezone = data.split(" ")
-    tz = pytz.timezone(f'{timezone}')
-    today = date.today()
-    now = datetime.now(tz)
-    time = now.strftime("%H:%M:%S %p")
-    await message.reply_text(
-        text=(f"today:{today}\ntime: {time}")
-    )
-@Client.on_message(filters.command('restart') & filters.private)
-async def start(bot, message):
-    msg = await message.reply_text(
-        text ='Proccesing'
-    )
-    lol = await User.get_chat_join_requests(chat_id=ACC_ALL_CHAT)
-                                
-    await msg.edit("completed")
-    await message.reply_text(f"total : {lol}")
-        
-    
-  
-
-
-@Client.on_message(filters.command('crgrp') & filters.private)
-async def start(bot, message):
-    await User.create_group("hi", users=ADMINS)
+@
     chnl = await User.create_channel("Channel Title", "Channel Description")
     await User.set_chat_photo(chat_id=chnl.id, photo="https://graph.org/file/0fab719eb1576a17830eb.jpg")
                              
@@ -259,37 +162,14 @@ async def pm_next_parge(bot, query):
     time = now.strftime("%I:%M:%S %p")
     await query.answer(f"Tᴏᴅᴀʏ 🎗 \n 🗓 : {today}", show_alert=True)
 
-@Client.on_message(filters.channel & (filters.document | filters.video | filters.audio)) 
-async def editing(bot, message):
-      try:
-         media = message.document or message.video or message.audio
-         caption_text = CAPTION_TEXT
-      except:
-         caption_text = ""
-         pass 
-      if (message.document or message.video or message.audio): 
-          if message.caption:                        
-             file_caption = f"**{message.caption}**"                
-          else:
-             fname = media.file_name
-             filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"  
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = caption_text + "\n" + file_caption,
-                 parse_mode = "markdown"
-             )
+
       
 Client.start()
 print("Bot Started!")
-User.start()
-print("User Started!")
 
 idle()
 
-User.stop()
-print("User Stopped!")
+
 Client.stop()
 print("Bot Stopped!")
     
