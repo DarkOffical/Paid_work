64f1535f87a61bb2b8b88ab0fa04b87d33da1c60
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-06 15:57:43 +0530

    Update approvedb.py

--- approvedb.py
+++ approvedb.py
@@ -1,55 +1,41 @@
-from pymongo import MongoClient
-from info import ACC_DB
+import motor.motor_asyncio
+from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, MAX_BTN, AUTO_FFILTER, SHORTLINK_API, SHORTLINK_URL, IS_SHORTLINK
 
-client = MongoClient(ACC_DB)
-
-users = client['main']['users']
-groups = client['main']['groups']
-time = client['main']['time']
-
-def already_db(user_id):
-        user = users.find_one({"user_id" : str(user_id)})
-        if not user:
-            return False
-        return True
-
-def already_dbg(chat_id):
-        group = groups.find_one({"chat_id" : str(chat_id)})
-        if not group:
-            return False
-        return True
-
-def set_del_time(del_time):
-    time.insert_one({"del_time": str(del_time)})
-
-def get_del_time():
-    get_time = time.find({})
-    return get_time
-
-def add_user(user_id):
-    in_db = already_db(user_id)
-    if in_db:
-        return
-    return users.insert_one({"user_id": str(user_id)}) 
-
-def remove_user(user_id):
-    in_db = already_db(user_id)
-    if not in_db:
-        return 
-    return users.delete_one({"user_id": str(user_id)})
+class Database:
     
-def add_group(chat_id):
-    in_db = already_dbg(chat_id)
-    if in_db:
-        return
-    return groups.insert_one({"chat_id": str(chat_id)})
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            ban_status=dict(
+                is_banned=False,
+                ban_reason="",
+            ),
+        )
 
-def all_users():
-    user = users.find({})
-    usrs = len(list(user))
-    return usrs
-
-def all_groups():
-    group = groups.find({})
-    grps = len(list(group))
-    return grps
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+        
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id':int(id)})
+        return bool(user)
+    
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+      
+    async def get_all_users(self):
+        return self.col.find({})
+    
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+     
+     
+    
+        
